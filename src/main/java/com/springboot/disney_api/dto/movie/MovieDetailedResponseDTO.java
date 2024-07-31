79fff3769a2c28b91ba8fa021270d34c021fe360
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2024-07-31 10:46:40 -0300

    refactor(movie): add relations to genres and characters to movie response

--- src/main/java/com/springboot/disney_api/dto/movie/MovieDetailedResponseDTO.java
+++ src/main/java/com/springboot/disney_api/dto/movie/MovieDetailedResponseDTO.java
@@ -1,12 +1,18 @@
 package com.springboot.disney_api.dto.movie;
 
+import com.springboot.disney_api.dto.character.CharacterResponseDTO;
+import com.springboot.disney_api.dto.genre.GenreResponseDTO;
+
 import java.time.LocalDate;
+import java.util.List;
 
 public record MovieDetailedResponseDTO(
         Long id,
         String title,
         String image,
         LocalDate creationDate,
-        Double rating
+        Double rating,
+        List<CharacterResponseDTO> characters,
+        List<GenreResponseDTO> genres
 ) {
 }
--- src/main/java/com/springboot/disney_api/service/MovieService.java
+++ src/main/java/com/springboot/disney_api/service/MovieService.java
@@ -1,22 +1,30 @@
 package com.springboot.disney_api.service;
 
+import com.springboot.disney_api.dto.character.CharacterResponseDTO;
+import com.springboot.disney_api.dto.genre.GenreResponseDTO;
 import com.springboot.disney_api.dto.movie.MovieDetailedResponseDTO;
 import com.springboot.disney_api.dto.movie.MovieRequestDTO;
 import com.springboot.disney_api.dto.movie.MovieResponseDTO;
 import com.springboot.disney_api.dto.movie.MovieUpdateDTO;
+import com.springboot.disney_api.model.Character;
+import com.springboot.disney_api.model.Genre;
 import com.springboot.disney_api.model.Movie;
+import com.springboot.disney_api.repository.GenreRepository;
 import com.springboot.disney_api.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
+    @Autowired
+    private GenreRepository genreRepository;
 
     public MovieResponseDTO createNewMovie(MovieRequestDTO movieDTO) {
         Movie movie = new Movie(movieDTO);
@@ -44,17 +52,30 @@ public class MovieService {
     }
 
     public MovieDetailedResponseDTO getMovieById(Long id) {
-        Optional<Movie> movieFound = movieRepository.findById(id);
-        if (movieFound.isPresent()) {
-            Movie movie = movieFound.get();
-            return new MovieDetailedResponseDTO(
-                    movie.getId(),
-                    movie.getTitle(),
-                    movie.getImage(),
-                    movie.getCreationDate(),
-                    movie.getRating()
-            );
-        } else return null;
+        Movie movie = movieRepository.findById(id).orElseThrow(() -> new RuntimeException("Movie not found"));
+        List<GenreResponseDTO> genres = movie.getGenres().stream().map(g -> new GenreResponseDTO(
+                g.getId(),
+                g.getName(),
+                g.getImage()
+        )).toList();
+        List<CharacterResponseDTO> characters = movie.getCharacters().stream().map(c -> new CharacterResponseDTO(
+                c.getId(),
+                c.getName(),
+                c.getImage(),
+                c.getAge(),
+                c.getWeight(),
+                c.getHistory()
+        )).toList();
+        return new MovieDetailedResponseDTO(
+                movie.getId(),
+                movie.getTitle(),
+                movie.getImage(),
+                movie.getCreationDate(),
+                movie.getRating(),
+                characters,
+                genres
+        );
+
     }
 
     public MovieResponseDTO updateMovie(Long id, MovieUpdateDTO body) {
